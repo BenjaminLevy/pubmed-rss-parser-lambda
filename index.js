e207342e494de736aa6980400f64b3e4f4b806fc
Benjamin <BnLvy1@gmail.com>
2023-04-01 16:30:36 -0700

    wip

--- index.js
+++ index.js
@@ -11,12 +11,36 @@ fs.readFile("./pubmed-api-response-limit-50.txt", "utf8", function(err, data){
    if(err){
     console.log(err)
   }else{
-    let jObj = parser.parse(data)
-    let content = jObj.rss.channel.item[1]["content:encoded"]
-    let jContent = parser.parse(content)
-    const portionOfXMLWithAbstract = jContent.div.p
+    // let fullXML = parser.parse(data)
+   // let content = jObj.rss.channel.item[1]["content:encoded"]
+    let jContent = parser.parse(data)
+    //console.log(jContent.rss.channel.item)
+    const portionOfXMLWithAbstract = jContent.rss.channel.item[0].description
+    console.log(portionOfXMLWithAbstract)
     console.log(portionOfXMLWithAbstract[portionOfXMLWithAbstract.length - 1]["#text"])
 
+    const singleArticle = jContent.rss.channel.item[0]
+    function getId(sourceArr, identifier){
+      fullId = sourceArr.find(a => a.startsWith(identifier))
+      idWithoutIdentifier = fullId.slice(identifier.length + 1)
+      return idWithoutIdentifier
+    }
+    console.log(getId(singleArticle['dc:identifier'], 'doi'))
   }
 })
+
+class Article{
+  constructor(data){
+    this.title = data.title
+    this.url = data.link
+    this.description = data.description
+    this.pmid = data.getId(data['dc:identifier'], "pmid")
+    this.doi = data.getId(singleArticle['dc:identifier'], 'doi')
+    this.date = data['dc:date']
+    this.journal = data['dc:source']
+  }
+  getId(sourceArr, identifier){
+    fullElement = sourceArr.find(a => a.startsWith(identifier))
+  }
+}
 //let jObj = parser.parse(XMLdata)
